11d1b4e1455aeb34ea2e62a023484448c8cd5dff
jzmnnava <146144314+jzmnnava@users.noreply.github.com>
2024-09-03 20:01:53 -0400

    Add files via upload
    File contains the Java code for Assignment 1-1

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+// This Program prints "Welcome to Java", "Welcome to Computer Science", and "Programming is fun". 
+public class Welcome {
+    public static void main(String[] args) {
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Computer Science");
+      System.out.println("Programming is fun");
+    }
+  }
+  
